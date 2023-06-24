0f755d805f30d3039ed43b1af261adde577c5801
Alexandr Leonov <as.leonov17@gmail.com>
2023-06-25 01:14:42 +0300

    In this commit: - Add Eureka server

--- config-service/src/main/java/ru/leonov/config/ConfigServiceApplication.java
+++ config-service/src/main/java/ru/leonov/config/ConfigServiceApplication.java
@@ -5,11 +5,13 @@ import io.sentry.SentryLevel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigServer
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 @SpringBootApplication
 public class ConfigServiceApplication {
